315e82383d404440cffcf27b6c8e27da9fdaad0a
kiplimo-dev <kiplimocollins855@gmail.com>
2023-01-07 22:57:02 +0300

    reformated code

--- src/main/java/com/collicode/security/security/config/WebSecurityConfig.java
+++ src/main/java/com/collicode/security/security/config/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.cs
+       http.
         super.configure(http);
     }
 }
