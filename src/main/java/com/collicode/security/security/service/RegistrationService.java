2730eebd414f7fb6cbf002d4b06d27a0209272d6
kiplimo-dev <kiplimocollins855@gmail.com>
2023-01-09 22:29:24 +0300

    dded some code

--- src/main/java/com/collicode/security/security/service/RegistrationService.java
+++ src/main/java/com/collicode/security/security/service/RegistrationService.java
@@ -4,6 +4,6 @@ import com.collicode.security.security.entity.RegistrationRequest;
 
 public class RegistrationService {
     public static String register(RegistrationRequest registrationRequest) {
-    // return  "Works";
+    return  "Works";
     }
 }
